ab0569f3f4c9554ee869aefc8df526acf15030f2
rahuljain0114 <rahuljain@roboticwares.com>
2017-06-20 12:04:31 +0530

    libraries upgraded

--- src/containers/Jobs.js
+++ src/containers/Jobs.js
@@ -1,7 +1,6 @@
 'use strict'
-import React, {Component} from 'react'
-import
-{
+import React, { Component } from 'react'
+import {
   StyleSheet,
   View,
   ScrollView,
@@ -10,19 +9,20 @@ import
   ListView,
   Platform,
   TouchableHighlight,
-  Text
+  FlatList,
+  ActivityIndicator 
 }
-from 'react-native'
+  from 'react-native'
 
-import { Form, Item, Input, Container, Content, Body, ListItem, CheckBox} from 'native-base';
+import { Form, Item, Input, Container, Content, ListItem, CheckBox, List, Body, Left, Right, Text, Header, Icon, Button } from 'native-base';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 var styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
-    flex: 1,
-    backgroundColor : '#f7f7f7',
-    marginBottom:Platform.OS === 'ios'? 60 : 50,
+    flex: 2,
+    backgroundColor: '#f7f7f7',
+    marginBottom: Platform.OS === 'ios' ? 60 : 50,
   },
   style_row_view: {
     flex: 1,
@@ -43,7 +43,7 @@ var styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    
+
   },
   listCircleCheckbox: {
     width: 64,
@@ -56,7 +56,10 @@ var styles = StyleSheet.create({
   hidden: {
     width: 0,
     height: 0
-  }
+  },
+  testsad: {
+    borderBottomWidth: 0
+  },
 })
 
 class Jobs extends Component {
@@ -64,19 +67,19 @@ class Jobs extends Component {
   constructor(props) {
     super(props);
 
-    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
       data: this._genRow(),
       dataSource: ds,
-      status:false
+      status: false
     }
   }
 
-  toggleStatus(){
+  toggleStatus() {
     this.setState({
-      status:!this.state.status
+      status: !this.state.status
     });
-    console.log('toggle button handler: '+ this.state.status);
+    console.log('toggle button handler: ' + this.state.status);
   }
 
   componentDidMount() {
@@ -85,22 +88,81 @@ class Jobs extends Component {
     });
   }
 
-  _genRow(){
+  _genRow() {
     var datas = [];
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 100; i++) {
       datas.push({
         row: i,
         isSelect: false,
+        id: i + 1000
       });
     }
     console.log('datas ' + JSON.stringify(datas));
     return datas;
   }
 
-  render () {
+  renderData = (item) => {
+    return (
+      <ListItem avatar>
+        <Left>
+          <Text> Hello </Text>
+        </Left>
+        <Body >
+          <Text adjustsFontSizeToFit>
+            {`${item.row} ${item.isSelect}`}
+          </Text>
+        </Body>
+        <Right >
+          <Text>Hi</Text>
+        </Right>
+      </ListItem>
+    )
+  }
+
+  /*renderHeader = () => {
+    return (
+      <Header>
+          <View style={{ backgroundColor: '#fff', flexGrow: .90, height: 40 }}>
+            <Input bordered='true' rounded style={{ fontSize: 14, backgroundColor: '#ffffff', borderColor: '#d3d3d3', borderWidth: 1 }}
+              placeholder="Search Reference No." />
+          </View>
+          <View style={{ backgroundColor: '#d7d7d7', flexGrow: .10, height: 40, alignItems: 'center', justifyContent: 'center' }}>
+            <Ionicons name='ios-barcode-outline' style={{ fontSize: 34 }} />
+          </View>
+      </Header>
+    );
+  };*/
+
+  renderFooter = () => {
+        if (!this.state.loading) {
+            return null
+        }
+
+        return (
+            <View
+                style={{
+                    paddingVertical: 20,
+                    borderTopWidth: 1,
+                    borderColor: '#CED0CE'
+                }}>
+                <ActivityIndicator animating size='large' />
+            </View>
+        )
+    }
+
+  render() {
     return (
       <View style={styles.container}>
-        <ListView
+        <List
+          style={{marginTop: 5, marginBottom: 50 }}>
+          <FlatList
+            data={this.state.data}
+            renderItem={({ item }) => this.renderData(item)}
+            keyExtractor={item => item.id}
+            ListFooterComponent={this.renderFooter}
+          />
+        </List>
+        {/*<ListView
           style={{marginTop: 5, marginBottom: 50}}
           dataSource = {this.state.dataSource}
           renderRow = {this._renderRow.bind(this)}
@@ -112,49 +174,49 @@ class Jobs extends Component {
               style={styles.style_separator}
               key={`${sectionID} - ${rowID}`}
             />}
-        />
-        <View style={{flex: 1, flexDirection: 'row', position: 'absolute', bottom: 5, marginLeft:5, marginRight: 5 }}>
-            <View style={{ backgroundColor: '#fff', flexGrow: .90, height: 40 }}>
-                <Input bordered='true' rounded style={{fontSize: 14, backgroundColor: '#ffffff', borderColor: '#d3d3d3', borderWidth: 1}} 
-            placeholder="Search Reference No." />
-            </View>
-            <View style={{ backgroundColor: '#d7d7d7', flexGrow: .10, height: 40, alignItems: 'center', justifyContent: 'center'  }}>
-              <Ionicons name='ios-barcode-outline' style={{ fontSize: 34}}/>
-            </View>
+        />*/}
+        <View style={{ flex: 1, flexDirection: 'row', position: 'absolute', bottom: 5, marginLeft: 5, marginRight: 5 }}>
+          <View style={{ backgroundColor: '#fff', flexGrow: .90, height: 40 }}>
+            <Input bordered='true' rounded style={{ fontSize: 14, backgroundColor: '#ffffff', borderColor: '#d3d3d3', borderWidth: 1 }}
+              placeholder="Search Reference No." />
+          </View>
+          <View style={{ backgroundColor: '#d7d7d7', flexGrow: .10, height: 40, alignItems: 'center', justifyContent: 'center' }}>
+            <Ionicons name='ios-barcode-outline' style={{ fontSize: 34 }} />
+          </View>
         </View>
       </View>
     )
   }
 
   _renderRow(rowData: string, sectionID: number, rowID: number) {
-   
+
     return (
-        <View style={styles.style_row_view}>
-          <TouchableHighlight underlayColor='#e7e7e7' style={[styles.listCircle, this.state.status ? styles.hidden : {}]} onPress={()=>this.toggleStatus()}>
-            <View>
-              <Text style={{fontSize: 14, color: '#ffffff', textAlign: 'center', fontWeight: 'bold', backgroundColor: 'transparent'}}>Del</Text>
-              <Text style={{fontSize: 11, marginTop: 5, textAlign: 'center', color: '#ffffff', width: 60}}>Run1234asd56</Text>
-            </View>
-          </TouchableHighlight>
-
-          <View style={[styles.listCircleCheckbox, !this.state.status ? styles.hidden : {}]} >
-            <CheckBox
-                size = {36}
-                icon_check= 'ios-checkmark-circle-outline'
-                icon_open= 'ios-radio-button-off'
-                text=''
-            />
+      <View style={styles.style_row_view}>
+        <TouchableHighlight underlayColor='#e7e7e7' style={[styles.listCircle, this.state.status ? styles.hidden : {}]} onPress={() => this.toggleStatus()}>
+          <View>
+            <Text style={{ fontSize: 14, color: '#ffffff', textAlign: 'center', fontWeight: 'bold', backgroundColor: 'transparent' }}>Del</Text>
+            <Text style={{ fontSize: 11, marginTop: 5, textAlign: 'center', color: '#ffffff', width: 60 }}>Run1234asd56</Text>
           </View>
+        </TouchableHighlight>
 
-          <TouchableHighlight underlayColor='#e7e7e7' style={{flex: 1, height: 64}} onPress={this._onPressRow.bind(this.rowID, rowData)}>
-            <View style={{padding: 5, paddingTop: 10, paddingLeft: 10}}>
-              <Text style={{fontSize: 14, color: '#000000'}}>{rowData.row}</Text>
-              <Text style={{marginTop: 10, color: '#666666'}}>{rowData.isSelect ? 'true' : 'false'}</Text>
-            </View>
-          </TouchableHighlight>
+        <View style={[styles.listCircleCheckbox, !this.state.status ? styles.hidden : {}]} >
+          <CheckBox
+            size={36}
+            icon_check='ios-checkmark-circle-outline'
+            icon_open='ios-radio-button-off'
+            text=''
+          />
         </View>
-      );
-    }
+
+        <TouchableHighlight underlayColor='#e7e7e7' style={{ flex: 1, height: 64 }} onPress={this._onPressRow.bind(this.rowID, rowData)}>
+          <View style={{ padding: 5, paddingTop: 10, paddingLeft: 10 }}>
+            <Text style={{ fontSize: 14, color: '#000000' }}>{rowData.row}</Text>
+            <Text style={{ marginTop: 10, color: '#666666' }}>{rowData.isSelect ? 'true' : 'false'}</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 
   _onPressRow(rowID, rowData) {
     console.log(rowID);
