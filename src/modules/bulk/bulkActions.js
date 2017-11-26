2bd8ce48c6d8540c9e2db9d90e1afe7d559e7f76
Udbhav Goyal <udbhav@roboticwares.com>
2017-11-27 00:03:14 +0530

    job master specific configuration done

--- src/modules/bulk/bulkActions.js
+++ src/modules/bulk/bulkActions.js
@@ -20,9 +20,11 @@ import {
     keyValueDBService
 } from '../../services/classes/KeyValueDBService'
 import {
-   BULK
+    BULK
 } from '../../lib/AttributeConstants'
- import { moduleCustomizationService } from '../../services/classes/ModuleCustomization'
+import {
+    moduleCustomizationService
+} from '../../services/classes/ModuleCustomization'
 
 
 export function getJobMasterVsStatusNameList() {
@@ -30,17 +32,22 @@ export function getJobMasterVsStatusNameList() {
         try {
             dispatch(setState(START_FETCHING_BULK_CONFIG))
             const jobMasterList = await keyValueDBService.getValueFromStore(JOB_MASTER)
-            if(!jobMasterList || !jobMasterList.value){
+            if (!jobMasterList || !jobMasterList.value) {
                 throw new Error('Job master missing')
             }
             const jobStatusList = await keyValueDBService.getValueFromStore(JOB_STATUS)
-            if(!jobStatusList || !jobStatusList.value){
+            if (!jobStatusList || !jobStatusList.value) {
                 throw new Error('Job status missing')
             }
-            const jobMasterVsStatusList = await bulkService.prepareJobMasterVsStatusList(jobMasterList.value, jobStatusList.value)
-            dispatch(setState(STOP_FETCHING_BULK_CONFIG,jobMasterVsStatusList))
+            const modulesCustomizationList = await keyValueDBService.getValueFromStore(CUSTOMIZATION_APP_MODULE)
+            if (!modulesCustomizationList || !modulesCustomizationList.value) {
+                throw new Error('Module customization missing')
+            }
+            const jobMasterVsStatusList = await bulkService.prepareJobMasterVsStatusList(jobMasterList.value, jobStatusList.value, modulesCustomizationList.value)
+            dispatch(setState(STOP_FETCHING_BULK_CONFIG, jobMasterVsStatusList))
         } catch (error) {
             console.log(error)
+              dispatch(setState(STOP_FETCHING_BULK_CONFIG,[]))
         }
     }
 }
@@ -52,13 +59,13 @@ export function getBulkJobTransactions(bulkParams) {
             dispatch(setState(START_FETCHING_BULK_TRANSACTIONS))
             const bulkTransactions = await bulkService.getJobListingForBulk(bulkParams)
             const modulesCustomizationList = await keyValueDBService.getValueFromStore(CUSTOMIZATION_APP_MODULE)
-           const bulkModule = await moduleCustomizationService.getModuleCustomizationForAppModuleId(modulesCustomizationList.value,BULK.appModuleId)
-            const selectAll = (bulkModule[0].remark)?JSON.parse(bulkModule[0].remark).selectAll:false
-            console.log('selectAll',selectAll)
-           dispatch(setState(STOP_FETCHING_BULK_TRANSACTIONS,{
-            bulkTransactions,
-            selectAll
-        }))
+            const bulkModule = await moduleCustomizationService.getModuleCustomizationForAppModuleId(modulesCustomizationList.value, BULK.appModuleId)
+            const selectAll = (bulkModule[0].remark) ? JSON.parse(bulkModule[0].remark).selectAll : false
+            console.log('selectAll', selectAll)
+            dispatch(setState(STOP_FETCHING_BULK_TRANSACTIONS, {
+                bulkTransactions,
+                selectAll
+            }))
         } catch (error) {
             console.log(error)
             dispatch(setState(STOP_FETCHING_BULK_TRANSACTIONS))
@@ -66,45 +73,44 @@ export function getBulkJobTransactions(bulkParams) {
     }
 }
 
-export function toggleListItemIsChecked(jobTransactionId,allTransactions){
-        return async function(dispatch){
-            try{
-                const bulkTransactions = await JSON.parse(JSON.stringify(allTransactions))
-                bulkTransactions[jobTransactionId].isChecked = !bulkTransactions[jobTransactionId].isChecked
-                const selectedItems = await bulkService.getSelectedTransactionIds(bulkTransactions)
-                dispatch(setState(TOGGLE_JOB_TRANSACTION_LIST_ITEM,{
-                    selectedItems,
-                    bulkTransactions
-                }))
-            }catch(error){
-                console.log(error)
-            }
+export function toggleListItemIsChecked(jobTransactionId, allTransactions) {
+    return async function (dispatch) {
+        try {
+            const bulkTransactions = await JSON.parse(JSON.stringify(allTransactions))
+            bulkTransactions[jobTransactionId].isChecked = !bulkTransactions[jobTransactionId].isChecked
+            const selectedItems = await bulkService.getSelectedTransactionIds(bulkTransactions)
+            dispatch(setState(TOGGLE_JOB_TRANSACTION_LIST_ITEM, {
+                selectedItems,
+                bulkTransactions
+            }))
+        } catch (error) {
+            console.log(error)
         }
+    }
 }
 
-export function toggleAllItems(allTransactions,selectAllNone){
-        return async function(dispatch){
-            try{
-                 const bulkTransactions = await JSON.parse(JSON.stringify(allTransactions))
-                 let selectedItems,displayText = ''
-                 if(selectAllNone=='Select All'){
-                    Object.values(bulkTransactions).forEach(bulkTransaction=>bulkTransaction.isChecked=true)
-                    selectedItems = Object.keys(allTransactions)
-                    displayText = 'Select None'
-                 }
-                 else{
-                     Object.values(bulkTransactions).forEach(bulkTransaction=>bulkTransaction.isChecked=false)
-                     selectedItems = []
-                      displayText = 'Select All'
-                 }
-                 
-                  dispatch(setState(TOGGLE_ALL_JOB_TRANSACTIONS,{
-                    selectedItems,
-                    bulkTransactions,
-                    displayText
-                }))
-            }catch(error){
-                console.log(error)
+export function toggleAllItems(allTransactions, selectAllNone) {
+    return async function (dispatch) {
+        try {
+            const bulkTransactions = await JSON.parse(JSON.stringify(allTransactions))
+            let selectedItems, displayText = ''
+            if (selectAllNone == 'Select All') {
+                Object.values(bulkTransactions).forEach(bulkTransaction => bulkTransaction.isChecked = true)
+                selectedItems = Object.keys(allTransactions)
+                displayText = 'Select None'
+            } else {
+                Object.values(bulkTransactions).forEach(bulkTransaction => bulkTransaction.isChecked = false)
+                selectedItems = []
+                displayText = 'Select All'
             }
+
+            dispatch(setState(TOGGLE_ALL_JOB_TRANSACTIONS, {
+                selectedItems,
+                bulkTransactions,
+                displayText
+            }))
+        } catch (error) {
+            console.log(error)
         }
+    }
 }
