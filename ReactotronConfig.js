92b073533d6c88a14ef36c59b8e240f0d41eab24
HarshulSharma0000 <harshul.sharma@getfareye.com>
2018-01-01 19:05:51 +0530

    Reactotronnnngit status!

---
+++ ReactotronConfig.js
@@ -0,0 +1,25 @@
+import Reactotron, {
+    trackGlobalErrors,
+    openInEditor,
+    overlay,
+    asyncStorage,
+    networking
+  } from 'reactotron-react-native'
+import { reactotronRedux } from 'reactotron-redux'
+console.tron =  Reactotron
+
+  
+Reactotron
+    .configure({
+        host: '192.168.31.34',
+        name: "Fareye App"
+    }) // controls connection & communication settings
+    //.use(reactotronRedux()) //  <- Redux like govt mind control experiments!!
+    .use(trackGlobalErrors({
+        veto: frame => frame.fileName.indexOf('/node_modules/react-native/') >= 0
+    }))
+    .use(openInEditor())
+    .use(overlay())
+    .use(asyncStorage())
+    .use(networking())
+    .connect() //let's connect!!!
