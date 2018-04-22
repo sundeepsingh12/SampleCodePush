0603a817fa8545ba5ea9c12836c9bed81558775f
Udbhav Goyal <udbhav@roboticwares.com>
2018-04-22 21:15:55 +0530

    project rename completed

--- ios/FareyeTests/FareyeTests.m
+++ ios/FareyeTests/FareyeTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface FareyeReactTests : XCTestCase
+@interface FareyeTests : XCTestCase
 
 @end
 
-@implementation FareyeReactTests
+@implementation FareyeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
