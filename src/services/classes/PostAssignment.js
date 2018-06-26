ef9d4045754c4b5b023a3acdef8bcb3abb960f33
Udbhav Goyal <udbhav@roboticwares.com>
2018-06-26 12:44:19 +0530

    sync flag time added for forced assignment and check in moment changed

--- src/services/classes/PostAssignment.js
+++ src/services/classes/PostAssignment.js
@@ -80,7 +80,8 @@ class PostAssignment {
         let transactionDTO = {
             id: jobTransaction.id,
             referenceNumber: jobTransaction.referenceNumber,
-            jobId: jobTransaction.jobId
+            jobId: jobTransaction.jobId,
+            syncTime: moment().format('YYYY-MM-DD HH:mm:ss')
         }
         const runSheet = await formLayoutEventsInterface._updateRunsheetSummary(jobTransaction.jobStatusId, pendingStatus.statusCategory, [jobTransaction])
         await formLayoutEventsInterface._updateJobSummary(jobTransaction, pendingStatus.id)
@@ -98,8 +99,7 @@ class PostAssignment {
         jobTransactionDTOMap[jobTransaction.id] = transactionDTO
         let jobTransactionTableDTO = {
             tableName: TABLE_JOB_TRANSACTION,
-            value: transactionList,
-            syncTime: moment().format('YYYY-MM-DD HH:mm:ss')
+            value: transactionList,   
         }
         realm.performBatchSave(jobTransactionTableDTO, runSheet)
         await formLayoutEventsInterface.addTransactionsToSyncList(jobTransactionDTOMap)
